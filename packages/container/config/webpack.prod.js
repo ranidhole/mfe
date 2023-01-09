8b02302ce5dccff4c010b36174d8ed3268f3c102
ranidhole <ranidhole@gmail.com>
2023-01-09 19:07:43 +0530

    main changes

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -8,8 +8,7 @@ const domain = process.env.PRODUCTION_DOMAIN;
 const prodConfig = {
     mode:'production',
     output: {
-        filename:'[name].[contenthash].js',
-        publicPath:'/container/latest/'
+        filename:'[name].[contenthash].js'
     },
     plugins:[
         new ModuleFederationPlugin({
